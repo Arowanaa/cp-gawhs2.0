ccfcd15d58a7e2430067106671561134d967c1c7
Arowanaa <63178596+Arowanaa@users.noreply.github.com>
2020-12-11 00:19:29 -0800

    Update sysctl.sh

--- PW/sysctl.sh
+++ PW/sysctl.sh
@@ -11,10 +11,24 @@ sudo chmod 777 /etc/sysctl.conf
 echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >> /etc/sysctl.conf
 echo "net.ipv4.conf.all.accept_redirects = 0" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.accept_redirects = 0" >> /etc/sysctl.conf
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward = 0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.all.accept_source_route = 0" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.accept_source_route = 0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.all.send_redirects = 0" >> /etc/sysctl.conf
+
+echo "net.ipv4.tcp_syncookies = 1"  >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.rp_filter = 1"  >> /etc/sysctl.conf
+echo "net.ipv4.tcp_synack_retries = 5"  >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rfc1337 = 1" >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.accept_redirects = 0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.default.accept_redirects = 0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.secure_redirects = 0"  >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.accept_redirects = 0"  >> /etc/sysctl.conf
+echo "net.ipv4.conf.default.secure_redirects = 0"  >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.accept_redirects = 0"  >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.accept_redirect = 0"  >> /etc/sysctl.conf
+echo "kernel.randomize_va_space = 1" >> /etc/sysctl.conf
+echo "net.ipv4.icmp_echo_ignore_all =  0"  >> /etc/sysctl.conf
 
 #securing 99-sysctl.conf
 echo "net.ipv4.conf.default.rp_filter=1" >> /etc/sysctl.d/99-sysctl.conf
