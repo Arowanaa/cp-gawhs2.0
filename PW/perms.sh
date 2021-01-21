ff9f90d92f4d1515204c4cb92c06c67ba7254147
Arowanaa <63178596+Arowanaa@users.noreply.github.com>
2021-01-21 12:03:18 -0800

    Update perms.sh

--- PW/perms.sh
+++ PW/perms.sh
@@ -2,3 +2,5 @@ chown root:root /etc/hosts.allow
 chmod 644 /etc/hosts.allow
 chown root:root /etc/hosts.deny
 chmod 644 /etc/hosts.deny
+chown root:root /etc/ssh/sshd_config
+chmod og-rwx /etc/ssh/sshd_config
