ace3312ae5c975a0f5d7c43dbd19d6b25b49416d
Arowanaa <63178596+Arowanaa@users.noreply.github.com>
2021-01-22 03:47:38 -0800

    Update automator.sh

--- PW/automator.sh
+++ PW/automator.sh
@@ -2,5 +2,6 @@
 sudo apt-get -y install libpam-pwquality
 sudo ./lightdm.sh
 sudo ./rootpwchange.sh
-sudo ./sysctl.sh
+sudo cat login.defs.mod > /etc/login.defs
+sudo cat sysctl.conf.old > /etc/sysctl.conf
 
